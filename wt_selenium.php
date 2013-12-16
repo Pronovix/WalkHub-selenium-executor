285aa66fd7e51385125aa21974cd6755214627e0
Kornel Lugosi <coornail@gmail.com>
2013-12-16 09:43:21 +0000

    Add OpenAuth authentication

--- wt_selenium.php
+++ wt_selenium.php
@@ -2,24 +2,31 @@
 require 'vendor/autoload.php';
 include_once 'walkthrough/connection.inc';
 include_once 'walkthrough/authenticator/basic.inc';
+include_once 'walkthrough/authenticator/oauth.inc';
 include_once 'walkthrough/commands.inc';
 
 $command_line = new Commando\Command();
 
 $command_line->option('username')
   ->aka('u')
-  ->describedAs('Drupal user name')
-  ->require();
+  ->describedAs('Drupal user name');
 
 $command_line->option('password')
   ->aka('p')
-  ->describedAs('Drupal password')
-  ->require();
+  ->describedAs('Drupal password');
 
 $command_line->option('walkhub_url')
   ->aka('w')
-  ->describedAs('Walkhub url.')
+  ->describedAs('Walkhub url')
   ->require();
+
+$command_line->option('consumer key')
+  ->aka('k')
+  ->describedAs('OAuth Consumer Key');
+
+$command_line->option('consumer secret')
+  ->aka('s')
+  ->describedAs('OAuth Consumer Secret');
 
 $command_line->argument()
   ->referToAs('Action')
@@ -46,13 +53,33 @@ $function_name = 'command__' . $command_line[0];
 if (function_exists($function_name)) {
   $endpoint = $command_line['walkhub_url'] . '/api/v2';
   $connection = new Walkthrough\Connection();
-  $connection->setEndpoint($endpoint);
 
-  // Basic http authentication.
-  $authenticator = new Walkthrough\Authenticator\Basic();
-  $authenticator->setUsername($command_line['username']);
-  $authenticator->setPassword($command_line['password']);
-  $authenticator->setEndpoint($endpoint);
+  $authenticator = NULL;
+
+  // Basic HTTP authentication.
+  if ($command_line['username'] && $command_line['password']) {
+    $authenticator = new Walkthrough\Authenticator\Basic();
+    $authenticator->setUsername($command_line['username']);
+    $authenticator->setPassword($command_line['password']);
+    $authenticator->setEndpoint($endpoint);
+    $connection->setEndpoint($endpoint);
+  }
+
+  // OAuth authentication.
+  if ($command_line['consumer_key'] && $command_line['consumer_secret']) {
+    $authenticator = new \Walkthrough\Authenticator\Oauth();
+    $authenticator->setConsumerKey($command_line['consumer_key']);
+    $authenticator->setConsumerSecret($command_line['consumer_secret']);
+    $authenticator->setEndpoint($endpoint);
+
+    $endpoint .= '/oauth';
+    $connection->setEndpoint($endpoint);
+  }
+
+  if ($authenticator === NULL) {
+    echo "Please set an authentication method.\n";
+    exit(2);
+  }
 
   $connection->setAuthenticator($authenticator);
 
